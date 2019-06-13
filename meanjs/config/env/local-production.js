83fb4b54a509cea22fbee47684d86be0cea2e6ee
Snehal Mundhe <m.snehal106@gmail.com>
2019-06-13 00:48:08 -0400

    Home in the Hub

---
+++ meanjs/config/env/local-production.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://homeinthehubbos:joRstB8CGfhLhMSrWZVjfcnXyjIUPH27KyvwvdLeafaHGU0OOTkVaQFKTlXAcvNir3UUi4ZHaZQJC4NQcsCHKQ==@homeinthehubbos.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
+  }
+};
